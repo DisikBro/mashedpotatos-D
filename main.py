05d00be4339997bd83ebdfe2b4c076d29aeec701
zajnetdinov <dzajnetdinov285@gmail.com>
2024-02-12 18:48:22 +0500

    роналду пушка

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ def index(title):
     return render_template('index.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('index.html', prof=prof, title="Тренировка")
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('index.html', list=list, title="Тренировка")
+
+
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080)
