79d68a2ceb26eeb92f7af5c21a57776fa8529ba3
zajnetdinov <dzajnetdinov285@gmail.com>
2024-02-12 19:21:15 +0500

    меси пушка

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/<title>')
@@ -17,6 +18,23 @@ def training(prof):
 @app.route('/list_prof/<list>')
 def list_prof(list):
     return render_template('index.html', list=list, title="Тренировка")
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    params = {
+        "title": 'Анкета',
+        "surname": 'вайт',
+        "name": 'вальтер',
+        "education": 'ничо такое',
+        "profession": 'пылесос',
+        "sex": 'мужик',
+        "motivation": 'ъы',
+        "ready": 'Готов'
+    }
+
+    return render_template('auto_answer.html', **params)
 
 
 if __name__ == '__main__':
